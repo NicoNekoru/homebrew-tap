0e467b2041c91f4b1a50aa9023bc846921483ba0
NicoNekoru <66227158+NicoNekoru@users.noreply.github.com>
2026-09-15 03:52:49 -0700

    Update tekai to 0.3.0

--- Formula/tekai.rb
+++ Formula/tekai.rb
@@ -1,8 +1,8 @@
 class Tekai < Formula
   desc "Self-contained, fidelity-preserving LaTeX engine and build system"
   homepage "https://github.com/NicoNekoru/tekai"
-  url "https://github.com/NicoNekoru/tekai/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "e018ddd98dd39538f6a48a27bccf88ecd42f7d9cd35545392818968b1250ddcc"
+  url "https://github.com/NicoNekoru/tekai/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "aab164f7dc9d8b44b47312ff0d4b3884f992bcb6f7c9a44efa8b83f3f713a232"
   license "MIT"
   head "https://github.com/NicoNekoru/tekai.git", branch: "main"
 
@@ -16,5 +16,20 @@ class Tekai < Formula
   test do
     assert_match "tekai #{version}", shell_output("#{bin}/tekai --version")
     assert_match "self-contained engine", shell_output("#{bin}/tekai --help")
+
+    ENV["TEKAI_ENGINE_CACHE"] = testpath/"cache"
+    ENV["PATH"] = ""
+    (testpath/"main.tex").write <<~'EOS'
+      \documentclass{article}
+      \usepackage{eso-pic,fancyhdr,times}
+      \begin{document}
+      {\scshape Conference title}\par
+      {\bfseries Anonymous authors}\par
+      {\itshape Italic text}
+      \end{document}
+    EOS
+    system bin/"tekai", "build", "main.tex", "--report-json"
+    assert_path_exists testpath/"build/main.pdf"
+    assert_match "utmb8a.pfb", (testpath/"build/main.log").read.gsub(/\s+/, "")
   end
 end
